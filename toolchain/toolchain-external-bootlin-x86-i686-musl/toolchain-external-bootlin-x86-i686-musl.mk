00953b252d36e15cfdd236ac1f2e9758c7e94f07
Vadim Kochan <vadim4j@gmail.com>
2019-11-18 03:12:13 +0200

    toolchain: add support for x86-i686 musl toolchain
    Add support of toolchain for x86-i686 platform build with:
    
        binutils    2.29.1
        gcc         7.3.0
        gdb         7.12.1
        linux       4.1.52
        musl        1.1.19
    
    Signed-off-by: Vadim Kochan <vadim4j@gmail.com>

---
+++ toolchain/toolchain-external-bootlin-x86-i686-musl/toolchain-external-bootlin-x86-i686-musl.mk
@@ -0,0 +1,11 @@
+################################################################################
+#
+# toolchain-external-bootlin-x86-i686-musl
+#
+################################################################################
+
+TOOLCHAIN_EXTERNAL_BOOTLIN_X86_I686_MUSL_VERSION = 2018.11-1
+TOOLCHAIN_EXTERNAL_BOOTLIN_X86_I686_MUSL_SITE = $(TOOLCHAIN_EXTERNAL_BOOTLIN_SITE)/x86-i686/tarballs
+TOOLCHAIN_EXTERNAL_BOOTLIN_X86_I686_MUSL_SOURCE = x86-i686--musl--stable-$(TOOLCHAIN_EXTERNAL_BOOTLIN_X86_I686_MUSL_VERSION).tar.bz2
+
+$(eval $(toolchain-external-package))
