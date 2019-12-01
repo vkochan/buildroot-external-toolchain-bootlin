831959843b3325af8bce36bae2df58cddfe0c250
Vadim Kochan <vadim4j@gmail.com>
2019-12-01 18:55:23 +0200

    toolchains: add toolchain for armv7 eabihf with musl
    Signed-off-by: Vadim Kochan <vadim4j@gmail.com>

---
+++ toolchain/toolchain-external-bootlin-armv7-eabihf-musl/toolchain-external-bootlin-armv7-eabihf-musl.mk
@@ -0,0 +1,11 @@
+################################################################################
+#
+# toolchain-external-bootlin-armv7-eabihf-musl
+#
+################################################################################
+
+TOOLCHAIN_EXTERNAL_BOOTLIN_ARMV7_EABIHF_MUSL_VERSION = 2018.11-1
+TOOLCHAIN_EXTERNAL_BOOTLIN_ARMV7_EABIHF_MUSL_SITE = $(TOOLCHAIN_EXTERNAL_BOOTLIN_SITE)/armv7-eabihf/tarballs
+TOOLCHAIN_EXTERNAL_BOOTLIN_ARMV7_EABIHF_MUSL_SOURCE = armv7-eabihf--musl--stable-$(TOOLCHAIN_EXTERNAL_BOOTLIN_ARMV7_EABIHF_MUSL_VERSION).tar.bz2
+
+$(eval $(toolchain-external-package))
