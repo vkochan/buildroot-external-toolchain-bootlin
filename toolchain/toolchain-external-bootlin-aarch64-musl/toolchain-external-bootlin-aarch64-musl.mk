c5c005b877217b2c425366aa3435256c38d64283
Vadim Kochan <vadim4j@gmail.com>
2019-12-02 01:40:33 +0200

    toolchain: add support for aarch64 platform with musl
    Signed-off-by: Vadim Kochan <vadim4j@gmail.com>

---
+++ toolchain/toolchain-external-bootlin-aarch64-musl/toolchain-external-bootlin-aarch64-musl.mk
@@ -0,0 +1,11 @@
+################################################################################
+#
+# toolchain-external-bootlin-aarch64-musl
+#
+################################################################################
+
+TOOLCHAIN_EXTERNAL_BOOTLIN_AARCH64_MUSL_VERSION = 2018.11-1
+TOOLCHAIN_EXTERNAL_BOOTLIN_AARCH64_MUSL_SITE = $(BOOTLIN_TOOLCHAINS_SITE)/aarch64/tarballs
+TOOLCHAIN_EXTERNAL_BOOTLIN_AARCH64_MUSL_SOURCE = aarch64--musl--stable-$(TOOLCHAIN_EXTERNAL_BOOTLIN_ARMV7_EABIHF_MUSL_VERSION).tar.bz2
+
+$(eval $(toolchain-external-package))
